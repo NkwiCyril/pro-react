8db564350f6fbf2c2791b5516abe70b6a5af22e4
NkwiCyril <akinimbomnkwi@gmail.com>
2024-09-24 08:47:31 +0100

    Conditional rendering and using the setInterval method

--- my-react-app/src/components/Counter.jsx
+++ my-react-app/src/components/Counter.jsx
@@ -1,8 +1,32 @@
+import React, { Component } from "react";
 
-function Counter(props) {
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+    this.keepCounting = this.keepCounting.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+
+  keepCounting() {
+    setInterval(() => {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+      }));
+    }, 1000);
+  }
+
+  render() {
     return (
-        <h1>Let us count: <em>{props.count}</em></h1>
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.keepCounting}>Start</button>
+      </div>
     );
+  }
 }
 
 export default Counter;
--- my-react-app/src/components/UserGreeting.jsx
+++ my-react-app/src/components/UserGreeting.jsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserGreeting extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoggedIn: false,
-      };
-      this.login = this.login.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.login = this.login.bind(this);
+  }
 
-    login() {
-        console.log('I have been clicked')
-        this.setState({ isLoggedIn: true });
-    }
+//   method to login user
+  login() {
+    console.log("I have been clicked");
+    this.setState({ isLoggedIn: true });
+  }
 
   render() {
+    let message = this.state.isLoggedIn ? (
+      <h1>Logged in successfully!</h1>
+    ) : (   
+      <h1>Welcome to our platform! Please login to continue</h1>
+    );
 
-    
-
-    return (  
-      <div>
-        
-      </div>
-    )
+    return <div>
+        {message}
+        <button type="button" onClick={this.login}>Log me</button>
+    </div>;
   }
 }
 
-export default UserGreeting
+export default UserGreeting;
