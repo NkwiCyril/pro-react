7f9be0c6b1b949eef52ff1ec91fab9c27fa8e059
NkwiCyril <akinimbomnkwi@gmail.com>
2024-09-26 10:57:54 +0100

    Using refs to access DOM nodes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,12 @@
+import Form from "./components/ControlledComponents/Form";
 import Counter from "./components/Counter";
 import EventClass from "./components/EventClass";
 import FormApp from "./components/Form/FormApp";
 import NameList from "./components/List/NameList";
+import Inline from "./components/List/Styling/Inline";
 import Message from "./components/Message";
 import ParentComponent from "./components/ParentComponent";
+import RefsDemo from "./components/RefsDemo";
 import UserGreeting from "./components/UserGreeting";
 
 
@@ -17,7 +20,10 @@ function App() {
       {/* <UserGreeting /> */}
       {/* <FormApp /> */}
       {/* <Counter /> */}
-      <NameList />
+      {/* <NameList /> */}
+      {/* <Inline primary={true}/> */}
+      {/* <Form /> */}
+      <RefsDemo />
     </div>
   );
 }
