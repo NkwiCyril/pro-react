14ce6a12ea61d529fbbac818de9d2942b03974b9
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-06 18:52:11 +0100

    UseState with prevState

--- my-react-app/src/components/Hooks/Counter.jsx
+++ my-react-app/src/components/Hooks/Counter.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementByFive = () => {
+    setCount((prevState) => prevState + 5);
+  };
 
   return (
     <div>
       <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(initialCount);
+        }}
+      >
+        Reset
+      </button>
       <button
         onClick={() => {
           setCount(count + 1);
@@ -20,6 +32,7 @@ function Counter() {
       >
         -
       </button>
+      <button onClick={incrementByFive}>+5</button>
     </div>
   );
 }
