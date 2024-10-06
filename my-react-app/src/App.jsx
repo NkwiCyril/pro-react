703157a9178453feab8886a6b88096a9dbc1013a
NkwiCyril <akinimbomnkwi@gmail.com>
2024-10-06 16:41:32 +0100

    Testing https requests uing json placeholder

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import Form from "./components/ControlledComponents/Form";
 import Counter from "./components/Counter";
 import EventClass from "./components/EventClass";
 import FormApp from "./components/Form/FormApp";
+import PostForm from "./components/HTTP/PostForm";
+import PostList from "./components/HTTP/PostList";
 import NameList from "./components/List/NameList";
 import Inline from "./components/List/Styling/Inline";
 import Message from "./components/Message";
@@ -23,7 +25,9 @@ function App() {
       {/* <NameList /> */}
       {/* <Inline primary={true}/> */}
       {/* <Form /> */}
-      <RefsDemo />
+      {/* <RefsDemo /> */}
+      {/* <PostList /> */}
+      <PostForm/>
     </div>
   );
 }
